15133d265b87fee7589cbe94d7bf9b3a4bfc3e4a
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-25 19:32:27 +0100

    [feat] chap8: suppression attributs de post-list-item inutiles, pour utilisation du model post

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -10,30 +10,21 @@ export class PostListItemComponent implements OnInit {
 
   @Input()
   post: Post;
-  
-  postTitle: string;
-  postContent: string;
-  postCreatedAt: Date;
-  loveIts: number;
 
   constructor() { 
   }
 
   ngOnInit() {
-    this.postTitle = this.post.title;
-    this.postContent = this.post.content;
-    this.postCreatedAt = this.post.created_at;
-    this.loveIts = this.post.loveIts;
   }
 
   onLike() {
-    this.loveIts++;
-    console.log('loveIts: ' + this.loveIts);
+    this.post.loveIts++;
+    console.log('loveIts: ' + this.post.loveIts);
   }
 
   onDontLike() {
-    this.loveIts--;
-    console.log('loveIts: ' + this.loveIts);
+    this.post.loveIts--;
+    console.log('loveIts: ' + this.post.loveIts);
   }
 
   getColor() {
