8069ce5f7ed382bb97a0c033af6828d6fc3cbef7
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-25 16:34:55 +0100

    [init] creation projet angular pour Openclassrooms (chapitre 4)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth = false;
+
+  appareilOne = "Machine à laver";
+  appareilTwo = "Télévision";
+  appareilThree = "Ordinateur";
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 
+      4000
+    ); 
+  }
+
+  onAllumer() {
+    console.log('On allume tout!');
+  }
 }
