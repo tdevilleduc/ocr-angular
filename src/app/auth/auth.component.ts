41b259f5da56bf521c04c78d78aec746ec225ba7
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-26 20:14:59 +0100

    [feat] chap10: redirection vers /appareils après l'authentification

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -10,7 +11,7 @@ export class AuthComponent implements OnInit {
 
   authStatus: boolean; 
 
-  constructor(private authService: AuthService ) { }
+  constructor(private authService: AuthService, private router: Router ) { }
 
   ngOnInit() {
     this.authStatus = this.authService.isAuth;
@@ -19,8 +20,8 @@ export class AuthComponent implements OnInit {
   onSignIn() {
     this.authService.signIn().then(
       () => {
-        console.log('connexion réussie !');
         this.authStatus = this.authService.isAuth;
+        this.router.navigate(['appareils']);
       }
     );
   }
