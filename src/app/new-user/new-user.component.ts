7eff5577b51e040fa73c0c4563e2d4117788c896
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-27 20:06:49 +0100

    [feat] chap13; ajout au formulaire new-user des hobbies, avec leur caractère list et facultatif; utilisation d'une methode de transtypage et d'un ajout de validator

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
@@ -26,7 +26,8 @@ export class NewUserComponent implements OnInit {
       firstName: ['', Validators.required],
       lastName: [, Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      drinkPreference: ['', Validators.required]
+      drinkPreference: ['', Validators.required],
+      hobbies: this.formBuilder.array( [] )
     });
   }
 
@@ -36,10 +37,20 @@ export class NewUserComponent implements OnInit {
       formValue['firstName'],
       formValue['lastName'],
       formValue['email'],
-      formValue['drinkPreference']
+      formValue['drinkPreference'],
+      formValue['hobbies'] ? formValue['hobbies'] : []
     );
     this.userService.addUser(newUser);
     this.router.navigate(['/users']);
+  }
+
+  getHobbies() {
+    return this.userForm.get('hobbies') as FormArray;
+  }
+
+  onAddHobby() {
+    const newHobbyControl = this.formBuilder.control('', Validators.required);
+    this.getHobbies().push(newHobbyControl);
   }
 
 }
