cd806fe8754b3840766b6f603a9b2bb5ab9ede59
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-27 15:41:14 +0100

    [feat] chap11: ajout d'un compteur du temps passé sur l'application (compteur infini)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  secondes: number;  
   constructor() {}
+
+  ngOnInit() {
+    const counter = interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: any) => {
+        console.log('Une erreur a été rencontrée !');
+      },
+      () => {
+        console.log('Observable completée');
+      }
+    );
+  }
   
 }
