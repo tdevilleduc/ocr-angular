8972e89612491b0450f04574ff94d7af48f24cfb
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-27 15:44:19 +0100

    [feat] chap11: ajout d'une souscription pour l'evenement et d'une methode onDestroy qui dessouscrit l'evenement a la destruction du component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   secondes: number;  
+  counterSubscription: Subscription;
+
   constructor() {}
 
   ngOnInit() {
     const counter = interval(1000);
-    counter.subscribe(
+    this.counterSubscription = counter.subscribe(
       (value: number) => {
         this.secondes = value;
-      },
-      (error: any) => {
-        console.log('Une erreur a été rencontrée !');
-      },
-      () => {
-        console.log('Observable completée');
       }
     );
   }
   
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
