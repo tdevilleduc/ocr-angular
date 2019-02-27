6b9387e49a5c3149ff44bd1c78de9f77b1c53bf3
tdevilleduc <devilleduc.thomas@gmail.com>
2019-02-27 17:32:37 +0100

    [feat] chap12; ajout d'une methode d'ajout d'appareil au service; branchement avec le formulaire d'ajout d'appareil; redirection vers la liste des appareils après l'ajout

--- src/app/edit-appareil/edit-appareil.component.ts
+++ src/app/edit-appareil/edit-appareil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AppareilService } from '../services/appareil.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-appareil',
@@ -10,12 +12,16 @@ export class EditAppareilComponent implements OnInit {
 
   defaultOnOff = 'éteint';
 
-  constructor() { }
+  constructor(private appareilService: AppareilService, 
+              private router: Router) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+  const name = form.value['name'];
+  const status = form.value['status'];
+  this.appareilService.addAppareil(name, status);
+  this.router.navigate(['/appareils']);
   }
 }
