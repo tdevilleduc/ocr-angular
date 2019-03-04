61db5e1ae7cf10a332b1233fcf66e07fc0fe1d51
tdevilleduc <devilleduc.thomas@gmail.com>
2019-03-04 13:49:51 +0100

    [tool] integration de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAJU5lkzybdRg8EkeA0QPxvXxHyyiZXHdM",
+      authDomain: "bookshelves-1391d.firebaseapp.com",
+      databaseURL: "https://bookshelves-1391d.firebaseio.com",
+      projectId: "bookshelves-1391d",
+      storageBucket: "",
+      messagingSenderId: "846632059001"
+    };
+    firebase.initializeApp(config);
+  }
 }
