02bad6728070f8d0be705ab7d5e49787a86efd3f
tdevilleduc <devilleduc.thomas@gmail.com>
2019-03-05 12:34:30 +0100

    [feat] chap15: ajout des fonctionnalités d'affichage d'une liste de livres, d'affichage d'un livre, d'ajout d'un nouveau livre, de suppression d'un livre existant

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Book } from '../models/book.model';
+import { Subscription } from 'rxjs';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription = new Subscription;
+
+  constructor(private bookService: BooksService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.booksSubscription = this.bookService.booksSubject.subscribe(
+      (books: Book[]) => {
+        this.books = books;
+      }
+    );
+    this.bookService.getBooks();
+    this.bookService.emitBooks();
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
+  }
+
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.bookService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
   }
 
 }
